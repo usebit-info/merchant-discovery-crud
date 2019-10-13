e5ba604972411f6ecae96f82d1d207d41830ab76
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-10-13 09:19:08 -0500

    renamed methods

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,12 @@ extern crate serde_derive;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Result};
 
-// serve the merchant form page
-// fn index() -> Result<HttpResponse> {
-// 	Ok(HttpResponse::Ok()
-// 		.content_type("text/html; charset=utf-8")
-// 		.body(include_str!("../site/index.html")))
-// }
-
 // the merchant
 #[derive(Serialize, Deserialize)]
 struct Merchant {
 	name: String,
 	url: String,
 }
-
-// add a merchant
-// fn merchants_add(params: web::Form<Merchant>) -> Result<HttpResponse> {
-// 	Ok(HttpResponse::Ok()
-// 		.content_type("text/plain; charset=utf-8")
-// 		.body(format!("name: {},\nurl: {}", params.name, params.url)))
-// }
 
 fn get_index() -> Result<HttpResponse> {
 	Ok(HttpResponse::Ok()
@@ -36,13 +22,27 @@ fn post_merchant(params: web::Form<Merchant>) -> Result<HttpResponse> {
 	.body(format!("name: {},\nurl: {}", params.name, params.url)))		
 }
 
+fn get_all_merchants(merchants: web::Data<Vec<Merchant>>) -> Result<HttpResponse> {
+	
+	// let mut mrch: String;
 
+	// for merchant in merchants {
+	// 	mrch.append(format!("{:?}", merchant))
+	// }
+
+	Ok(HttpResponse::Ok()
+		.content_type("text/plain; charset=utf-8")
+		.body(""))
+}
 
 fn main() {
+
+	//let mut merchants: Vec<Merchant> = Vec::with_capacity(8);
+
     HttpServer::new(|| {
     	App::new()
     		.route("/", web::get().to(get_index))
-    		.route("/merchants/add", web::post().to(post_merchant))
+    		.route("/merch/add", web::post().to(post_merchant))
     })
     .bind("127.0.0.1:8000")
     .unwrap()
